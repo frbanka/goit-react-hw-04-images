12ca1e76d5117eb671846774dcd6129a5766b1ea
Jacek Wójciak <fr.banka@gmail.com>
2023-04-14 20:11:44 +0200

    modal hook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,31 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
-class Modal extends Component {
-  static propsTypes = {
-    modalClose: PropTypes.func.isRequired,
-    children: PropTypes.node.isRequired,
-  };
+const Modal = ({ modalClose, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', whenKeyDown);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.whenKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.whenKeyDown);
-  }
-  whenKeyDown = event => {
+    return () => {
+      window.removeEventListener('keydown', whenKeyDown);
+    };
+  });
+  const whenKeyDown = event => {
     if (event.code === 'Escape') {
-      this.props.modalClose();
+      modalClose();
     }
   };
-  closeOnClick = event => {
+  const closeOnClick = event => {
     if (event.target === event.currentTarget) {
-      this.props.modalClose();
+      modalClose();
     }
   };
 
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <div
-        className={css.overlay}
-        onClick={this.closeOnClick}
-        role="presentation"
-      >
-        <div className={css.modal}>{children}</div>
-      </div>,
-      document.getElementById('root')
-    );
-  }
-}
+  return createPortal(
+    <div className={css.overlay} onClick={closeOnClick} role="presentation">
+      <div className={css.modal}>{children}</div>
+    </div>,
+    document.getElementById('root')
+  );
+};
 export default Modal;
